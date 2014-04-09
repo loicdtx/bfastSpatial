faf59beb9c9992d0616764c0c8c91a1d4a419c32
Loic <dutri001@papaya.(none)>
2014-04-09 15:54:52 +0200

    Added rd file (with roxygen2 in test_data.R) for the 3 landsat like tarballs

--- R/processLandsatBatch.R
+++ R/processLandsatBatch.R
@@ -1,19 +1,31 @@
 
 #' @title Process Landsat data in batch mode
 #' 
-#'@description Batcher to process Landsat data from tarball or hdf to a list of Vegetation Index files. Runs \link{processLandsat} sequentially or in parallel
+#' @description Batcher to process Landsat data from tarball or hdf to a list of Vegetation Index files. Runs \link{processLandsat} sequentially or in parallel
 #' 
 #'
-#'@param x Character. Directory where the data is located (hdf or tar.gz)
-#'@param pattern. Only useful if x if of length 1. See \link{list.files} for more details
-#'@param outdir. Character. Directory where the vegetation index rasterLayer should be written.
-#'@param hdfdir Character. Directory where the tarball should be uncompressed. Can be ommited if \code{untar} is set to \code{FALSE}
-#'@param mc.cores Numeric. For multicore implementation only. See \link{mclapply}
-#'@param ... Arguments to be passed to \link{processLandsat} (\code{untar}, \code{delete}) or to \link{hdf2ndvi} (\code{e}, \code{mask}, \code{keep})
-#'@author Loic Dutrieux
-#'@return Function is used for its side effect of calculating in batch Vegetation indices fron surface reflectance Lantsat data.
-#'@seealso \link{processLandsat} and \link{hdf2ndvi}
-#'@import stringr
+#' @param x Character. Directory where the data is located (hdf or tar.gz)
+#' @param pattern. Only useful if x if of length 1. See \link{list.files} for more details
+#' @param outdir. Character. Directory where the vegetation index rasterLayer should be written.
+#' @param hdfdir Character. Directory where the tarball should be uncompressed. Can be ommited if \code{untar} is set to \code{FALSE}
+#' @param mc.cores Numeric. For multicore implementation only. See \link{mclapply}
+#' @param ... Arguments to be passed to \link{processLandsat} (\code{untar}, \code{delete}) or to \link{hdf2ndvi} (\code{e}, \code{mask}, \code{keep})
+#' @author Loic Dutrieux
+#' @return Function is used for its side effect of calculating in batch Vegetation indices fron surface reflectance Lantsat data.
+#' @seealso \link{processLandsat} and \link{hdf2ndvi}
+#' @example
+#' # Get the directory where the Landsat archives are storred
+#' dir <- system.file('external', package='bfastSpatial')
+#' hdfdir <- dirout <- rasterOptions()$tmpdir
+#' processLandsatBatch(x=dir, pattern=glob2rx('*.tar.gz'), outdir=dirout, hdfdir=hdfdir, delete=TRUE, mask=16)
+#' 
+#' # Visualize one of the layers produced
+#' 
+#' 
+#' 
+#' 
+#' 
+#' @import stringr
 #' @import raster
 #' @import rgdal
 #' @import gdalUtils
---
+++ R/test_data.R
@@ -0,0 +1,8 @@
+#' Landsat-like tarballs
+#' @name LE72220622001176EDC01-LE72220622002147EDC00-LT52220622004289CUB01
+#' @description subset of Landsat surface reflectance data for testing pre-processing steps.
+#' @docType data
+#' @author NASA/USGS
+#' @keywords data
+
+NULL
