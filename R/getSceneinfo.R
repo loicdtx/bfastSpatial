b32cd80fac645116dae80ab5696585ff0b60b201
Loïc (from home) <loic.dutrieux@gmail.com>
2014-04-13 17:56:02 +0200

    getSceneinfo can take any character or list of character. Stop replaced by warning. Example added using tura dataset and what would come out of list.file() call

--- R/getSceneinfo.R
+++ R/getSceneinfo.R
@@ -7,17 +7,27 @@
 #' @param ... Additional arguments to pass to \code{\link{write.csv}}.
 #' @author Ben DeVries \email{devries.br@@gmail.com}
 #' @return a \code{data.frame} with parsed scene information from Landsat scene names
+#' @import stringr
+#' @examples 
+#' getSceneinfo(c('ndvi.LC82300702014234.tar.gz', 'ndvi.LT52300702008234.tif'))
+#' 
+#' # Load tura
+#' data(tura)
+#' getSceneinfo(names(tura))
 #' @export
 
 
 
-getSceneinfo <- function(sourcefile, filename=NULL, ...)
+getSceneinfo <- function(sourcefile, ...)
 {
   # for the sake of convenience, sourcefile can be either a character vector of scene names (or subfolders) or the original .tar.gz or .tar files
   # this will check which it is and format accordingly
-  sourcefile <- sapply(sourcefile, FUN=function(x){if(grepl(".gz", x)) x <- substr(x, 1, nchar(x)-3);
-                                                   if(grepl(".tar", x)) x <- substr(x, 1, nchar(x)-4); 
-                                                   return(x)})  
+  if(!all(grepl(pattern='(LT4|LT5|LE7|LC8)\\d{13}', x=sourcefile)))
+      warning('Some of the characters provided do not contain recognized Landsat5/7/8 scene ID')
+    
+    
+  sourcefile <- str_extract(sourcefile, '(LT4|LT5|LE7|LC8)\\d{13}') 
+  
   
   # dates in LS naming system are formatted with year and julian day as one number - "%Y%j" (e.g. 2001036 = 2001-02-05)
   # reformat date as "%Y-%m-%d" (ie. yyyy-mm-dd)
@@ -26,16 +36,15 @@ getSceneinfo <- function(sourcefile, filename=NULL, ...)
   # identify the sensor
   sensor <- as.character(mapply(sourcefile, dates, FUN=function(x,y){
     sen <- substr(x, 1, 3)
-    if(sen == "LE7" & y <= "2003-03-31")
+    if(is.na(sen)) NA 
+    else if(sen == "LE7" & y <= "2003-03-31")
       "ETM+ SLC-on"
     else if(sen == "LE7" & y > "2003-03-31")
       "ETM+ SLC-off"
     else if(sen == "LT5" | x == "LT4") 
       "TM" 
     else if(sen == "LC8")
-      "OLI"
-    else
-      stop(x, " is not a recognized Landsat5/7/8 scene ID.", call. = FALSE)
+      "OLI"      
   }))
   # extract path, row
   path <- as.numeric(substr(sourcefile, 4, 6))
@@ -43,6 +52,7 @@ getSceneinfo <- function(sourcefile, filename=NULL, ...)
   
   # throw all attributes into a data.frame
   info <- data.frame(sensor = sensor, path = path, row = row, date = dates)
+  sourcefile[is.na(sourcefile)] <- 'Not recognised'
   row.names(info) <- sourcefile
   
   # optional: print to .csv for future reference
