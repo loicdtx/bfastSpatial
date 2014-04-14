675a784657ae52aa65e419ef4994fe085feed4ca
Ben DeVries <devries.br@gmail.com>
2014-04-14 15:24:44 +0200

    annualSummary finished with documentation

--- R/annualSummary.R
+++ R/annualSummary.R
@@ -7,16 +7,27 @@
 #' @param sceneID Character. Optional: Landsat scene ID's for each layer of the input RasterBrick or RasterStack. If not given, sceneID's must be contained in the layer names
 #' @param sensor Character. Optional: limit calculation to images from a particular sensor. Defaults to "all", but can take any of "TM", "ETM+", "ETM+ SLC-off" or "ETM+ SLC-on"
 #' @param ... Arguments to be passed to \link{\code{mc.calc}}
+#' @return RasterBrick with results of fun for each year represtented in the input time series RasterBrick.
 #' 
 #' @author Ben DeVries \email{devries.br@@gmail.com}
+#' 
+#' @import raster
+#' @export
 #' 
 #' @examples
 #' # load tura RasterBrick
 #' data(tura)
 #' 
-#' # calculate mean value per year for ETM+ data only
-#' annualMean <- annualSummary(tura, fun=mean)
+#' # calculate mean and standard deviation values per year for ETM+ data only
+#' annualMean <- annualSummary(tura, fun=mean, na.rm=TRUE, sensor="ETM+")
+#' plot(annualMean)
+#' annualSD <- annualSummary(tura, fun=sd, na.rm=TRUE, sensor="ETM+")
+#' plot(annualSD)
 #' 
+#' # custom function to calculate # of non-NA values per pixel per year (similar to countObs())
+#' ff <- function(x)
+#'  length(x[!is.na(x)])
+#' annualObs <- annualSummary(tura, fun=ff, sensor="ETM+")
 
 annualSummary <- function(x, fun, sceneID=NULL, sensor="all", na.rm=NULL, ...){
     ###### na.rm doesn't work now for sum() or mean(), etc...!
