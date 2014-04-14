e317738ef95a4550883c93e63d7dc7a45d1cdfb7
Ben DeVries <devries.br@gmail.com>
2014-04-14 13:33:01 +0200

    Started annualSummary() function to calculate ts stats per year

---
+++ R/annualSummary.R
@@ -0,0 +1,43 @@
+#' @title Annual summary of a time series RasterBrick
+#' 
+#' @description Calculates pixel-based statistics for every year represented by a time series RasterBrick
+#' 
+#' @param RasterBrick or RasterStack
+#' @param fun Function to apply over each pixel for each year
+#' @param sceneID Character. Optional: Landsat scene ID's for each layer of the input RasterBrick or RasterStack. If not given, sceneID's must be contained in the layer names
+#' @param sensor Character. Optional: limit calculation to images from a particular sensor. Defaults to "all", but can take any of "TM", "ETM+", "ETM+ SLC-off" or "ETM+ SLC-on"
+#' @param ... Arguments to be passed to \link{\code{mc.calc}}
+#' 
+
+annualSummary <- function(x, fun, sceneID=NULL, sensor="all", ...){
+
+    # get scene information from layer names
+    if(is.null(sceneID)){
+        s <- getSceneinfo(names(x))
+    } else {
+        s <- getSceneinfo(sceneID)
+        names(x) <- row.names(s)
+    }
+    
+    # include data only from desired sensor(s) and update s accordingly
+    if (sensor != "all") {
+        if ("ETM+" %in% sensor) {
+            sensor <- unique(c(sensor, "ETM+ SLC-on", "ETM+ SLC-off"))
+        }
+        x <- dropLayer(x, which(!s$sensor %in% sensor))
+        s <- s[which(s$sensor %in% sensor), ]
+        names(x) <- row.names(s)
+    }
+    
+    # add year column to s
+    s$year <- as.numeric(substr(s$date, 1, 4))
+    years <- unique(s$year)
+    
+    # function to be applied over each pixel in the RasterBrickStack
+    pixStat <- function(b){
+        ps <- numeric()
+        for(i in 1:length(years)){
+            ps[i] <- 
+        }
+    }
+}
