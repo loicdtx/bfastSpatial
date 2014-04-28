59696dbff9bc329f283b3f6e727a40ce860d63c4
Ben DeVries <devries.br@gmail.com>
2014-04-28 16:10:49 +0200

    adding an option to summarize clump sizes

--- R/clumpSize.R
+++ R/clumpSize.R
@@ -4,37 +4,44 @@
 #' 
 #' @param x RasterLayer.
 #' @param f Numeric. Optional: conversion factor for number of pixels (e.g. f=900/10000 for converting Landsat pixels to area in hectares).
+#' @param stats Logical. Include summary statistics per clump? If TRUE, a list with (1) a raster layer showing clump sizes and (2) a summary table per clump. If FALSE, only a raster layer is returned.
 #' @param ... Additional arguments to pass to \code{\link{clump}}
 #' 
 #' @import raster
 #' @import igraph
-#' @author Ben DeVries
+#' @author Ben DeVries (devries.br@@gmail.com)
 #' @export
 
-clumpSize <- function(x, f=1, ...){
-  # groups raster cells into clumps using clump()
-  # assigns area (as # of pixels) to each clump
-  # args:
-  # x - raster layer
-  # f - conversion factor for # of pixels (e.g. 900/10000 for landsat pixels --> hectares)
-  # ... - arguments passed to clump()
-  
-  # identify clumps
-  y <- clump(x, ...)
-  
-  # make a reclass matrix from the frequency table of y
-  rcl <- freq(y)
-  rcl <- cbind(rcl[,1], rcl)
-  
-  # reclassify y based on rcl
-  z <- reclassify(y, rcl=rcl, right=NA)
-  
-  # remove NAs
-  z[is.na(y)] <- NA
-  
-  # conversion to other unit if factor is supplied
-  if(f != 1)
-    z <- z * f
-  
-  return(z)
+clumpSize <- function(x, f=1, stats=FALSE, ...){
+    # groups raster cells into clumps using clump()
+    # assigns area (as # of pixels) to each clump
+    # args:
+    # x - raster layer
+    # f - conversion factor for # of pixels (e.g. 900/10000 for landsat pixels --> hectares)
+    # ... - arguments passed to clump()
+    
+    # identify clumps
+    y <- clump(x, ...)
+    
+    # make a reclass matrix from the frequency table of y
+    rcl <- freq(y)
+    
+    # conversion to other unit if factor is supplied
+    if(f != 1)
+        rcl[, 2] <- rcl[, 2] * f
+    
+    # optional: make a summary matrix
+    if(stats){
+      sumstat <- matrix(nc=1, nr=7, dimnames=list(c("Mean", "Min.", "1st Qu.", "Median", "3rd Qu.", "Max.", "NA's"), c("clump size")))
+      sumstat[1,1] <- mean()
+    }
+    
+    # reclassify y based on rcl
+    rcl <- cbind(rcl[,1], rcl) # double first column (see ?reclassify)
+    z <- reclassify(y, rcl=rcl, right=NA)
+    
+    # remove NAs
+    z[is.na(y)] <- NA
+    
+    return(z)
 }
