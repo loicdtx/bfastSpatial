bb3aacb1f32a6b4e99551d03772d81f9299bdbfb
Loïc <loic.dutrieux@gmail.com>
2014-11-03 17:04:04 +0100

    Documented getMODISinfo

--- R/getMODISinfo.R
+++ R/getMODISinfo.R
@@ -1,10 +1,26 @@
 #' Get MODIS infos
 #' 
+#' @description Parses through MODIS file names and retrieve information on acquisition date. Future versions will include retrieval of sensor information, tile ID, etc... The function is vectorized.
+#' 
 #' @param x Character. MODIS file name
 #' 
-#' @description Initial version of function to parse through MODIS file names and retrieve information on acquisition date, sensor, tile number, etc... This initial version only includes date retrieval.
+#' @return
+#' A dataframe with one column ($date) of class \code{Date}
 #' 
 #' @author Loic Dutrieux
+#' 
+#' @examples
+#' # Simple case
+#' fileName <- '/path/to/file/MOD17A2.A2008097.h11v09.005.2008142064019.hdf'
+#' getMODISinfo(fileName)
+#' 
+#' # Multile files
+#' fileNames <- c('/path/to/file/MOD17A2.A2008097.h11v09.005.2008142064019.hdf', 'MOD17A2.A2008097.h11v09.005.2008164064019.tif')
+#' getMODISinfo(fileNames)
+#' 
+#' # Get only a vector of dates
+#' getMODISinfo(fileNames)$date
+#' 
 #' 
 #' @import stringr
 #' @export
