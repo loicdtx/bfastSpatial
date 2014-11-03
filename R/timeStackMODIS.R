036e523c672c0497f297884b93aa67a3ec3ddc93
Loïc <loic.dutrieux@gmail.com>
2014-11-03 13:58:14 +0100

    TimeStackMODIS function ready

---
+++ R/timeStackMODIS.R
@@ -0,0 +1,29 @@
+#' @title Creates a time stack of Landsat layers
+#'
+#' @description Creates a stack of MODIS layers, with time written to z dimension
+#'
+#' @param x character. dir containing the files to be stacked or character list (the files). IN the former case, it is recommended to use the \code{pattern} argument
+#' @param pattern See \link{list.files}
+#' @param ... Arguments to be passed to \link{writeRaster}. If specifying a filename, it is strongly recommended to also set a datatype.
+#' @author Loic Dutrieux
+#' 
+#' @import raster
+#' @export
+#'
+timeStackMODIS <- function(x, pattern=NULL, ...) {
+    if(!is.character(x)){
+        stop('x must be a character (directory) or a list of characters')
+    }
+    if (length(x) == 1){
+        x <- list.files(x, pattern=pattern, full.names=TRUE)
+    }
+    
+    s <- stack(x)
+    time <- getMODISinfo(x)$date
+    s <- setZ(x=s, z=time)
+    if(hasArg(filename)) {
+        out <- writeRaster(s, ...)
+        return(out)
+    }
+    return(s)
+}
