628d096585112ae256a419870d4de7f214f966fd
Loïc Dutrieux <loic.dutrieux@gmail.com>
2014-11-25 18:59:26 +0100

    Function to run bfastmonitor on a (multiple) zoo time-series. And return a dataframe

--- R/bfmZoo.R
+++ R/bfmZoo.R
@@ -1,5 +1,25 @@
-bfmZoo <- function(x, start, mc.cores = 1) {
+#' Runs bfastmonitor on a zoo object
+#' 
+#' @description The zoo object may contain several time-series
+#' 
+#' @param x A zoo object that may contain several time-series
+#' @param mc.cores Numeric For parallel processing. Number of workers.
+#' @param ... Arguments to be passed to \link{bfastmonitor}
+#' 
+#' @return A dataframe
+#' 
+#' @author Loic Dutrieux
+#' 
+#' @import bfast
+#' 
+#' @export
+#' 
+
+
+bfmZoo <- function(x, mc.cores = 1, ...) {
     
-    
-    mclapply(X = ts, FUN = bfastmonitor, start = start, mc.cores = mc.cores)
+    out <- mclapply(X = ts, FUN = bfastmonitor, mc.cores = mc.cores, ...)
+    # Unlist
+    data.frame(breakpoint = sapply(X = out, function(x) x$breakpoint),
+               magnitude = sapply(out, function(x) x$magnitude))
 }
