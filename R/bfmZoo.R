258e7f4cd655169b0bae82fcbf9695f9637971da
unknown <dutri001@L0116984.wurnet.nl>
2014-11-28 15:40:26 +0100

    Extented bfmZoo documentation

--- R/bfmZoo.R
+++ R/bfmZoo.R
@@ -1,6 +1,6 @@
 #' Runs bfastmonitor on a zoo object
 #' 
-#' @description The zoo object may contain several time-series
+#' @description This function is analog to the \link{bfastmonitor} function, but differs in terms of inputs. In \link{bfastmonitor} an object of class ts needs to be provided, usually pre-processed using \link{bfastts}; the present function accepts directly zoo time-series. The return also differs from \link{bfastmonitor}; instead of returning an object of class 'bfastmonitor'the present function returns a dataframe. The zoo object may contain several time-series, which results in a return dataframe containing several rows.
 #' 
 #' @param x A zoo object that may contain several time-series
 #' @param mc.cores Numeric For parallel processing. Number of workers.
