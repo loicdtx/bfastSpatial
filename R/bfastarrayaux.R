cf433e387174f440709badf785635cd65280677d
Meng Lu <lumeng0312@gmail.com>
2014-12-05 18:47:27 +0100

    add bfast breakpoints

--- R/bfastarrayaux.R
+++ R/bfastarrayaux.R
@@ -3,11 +3,39 @@ timeasdim<-function(newarr,timearr,variablearr)
   for (x in 1:dim(timearr)[1])
   {
     for (y in 1: dim(timearr)[2])
+     
+      {
+      if (!is.na(timearr[x,y]))
       newarr[x,y,timearr[x,y]]<- variablearr[x,y]
-  }
+    }
+    }
   return(newarr)
   
 }  
+
+timeasdimbfa<-function(newarr,timearr,variablearr) # for bfast
+{
+  for (x in 1:dim(timearr)[1])
+  {
+    for (y in 1: dim(timearr)[2])
+      
+    {
+      nb<-length(unlist(timearr[x,y]))
+     # if (  nb ==1)
+
+      #  newarr[x,y,timearr[x,y]]<- variablearr[x,y]
+      if ( nb >=1)
+      {
+        for(i in 1:nb)
+        newarr[x,y,unlist(timearr[x,y])[i]]<- variablearr[x,y]   
+    
+      }
+    }
+  }
+  return(newarr)  
+}  
+
+
 
 monthofyears<-function(x)
 {
