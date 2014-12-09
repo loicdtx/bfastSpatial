e1e31a605576acb90f31db50b054688ab5cd566c
Meng Lu <lumeng0312@gmail.com>
2014-12-09 12:06:25 +0100

    try bfast bfast monitoring on array

--- trythings.R
+++ trythings.R
@@ -8,9 +8,9 @@ list.files()
 load("a1.saved")
 load("fevi8.Rdata")
 
-fevi20b20<-fevi8[1:20,1:20,] # input example array
+fevi20b20<-fevi8[20:50,20:50,] # input example array
  
-output1<-bfmarray(fevi9b9,dates=a1,aggre='month',start=9) # bfast monitor
+output1<-bfmarray(fevi20b20,dates=a1,aggre='month',start=9) # bfast monitor
 
 ## convert time to time dimension
 
@@ -31,15 +31,37 @@ t3darr<-timeasdim(newarray,mtime,mag) # 3 dimensional array with magnitude
 which(!is.na(t3darr),arr.ind=TRUE)
 image(t3darr[,,176]) #test: bfastmonitor magnitude
 
-#test bfast
-output2 <-bfaarray(fevi9b9,dates=a1,aggre='month',season="harmonic",max.iter=3)
+#test bfast: save only time
+t1<-fevi9b9[1:2,1:2,]
+output3 <-bfaarray(t1,dates=a1,aggre='month',season="harmonic",max.iter=1,level=0.05)
 dimt=23*12
 dimx<-20
 dimy<-20
+
 newarray<-array(,c(dimx,dimy,dimt)) #newarr
  
 mag<- array(100,c(20,20))
  
-t3dbfaarr<-timeasdimbfa(newarray,app1,mag)
+t3dbfaarr<-timeasdimbfa(newarray,output3,mag)
 which(!is.na(t3dbfaarr),arr.ind=TRUE)
- 
+########################################### bfast with save other values
+output2 <-bfaarray2(t1,dates=a1,aggre='month',season="harmonic",max.iter=1,level=0.05)
+str(output2)
+allbreakpoints<-output2[1:6,,] # breakpoint
+allmagnitudes<-output2[7:12,,] # magnitude
+which(allbreakpoints!=0)
+ mtime<-allbreakpoints #timearr
+
+dimx<-dim(output2)[2]
+dimy<-dim(output2)[3]
+dimt<-max(mtime[ mtime!=0])
+
+newarray<-array(,c(dimx,dimy,dimt)) #newarr
+
+mag<- allmagnitudes  # variablearr (magnitude)
+
+t3darrbfamul<-tasd.bfa.mul(newarray,mtime,mag) # 3 dimensional array with magnitude 
+
+which(!is.na(t3darrbfamul),arr.ind=TRUE)
+image(t3darrbfamul[,,140]) #test: bfastmonitor magnitude
+t3darrbfamul[1,17,140]
