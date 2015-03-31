f643bbf882d1e97148ae11ac88a04a67f11379fb
Loïc Dutrieux <loic.dutrieux@gmail.com>
2015-03-31 11:37:27 +0200

    First trial at implementing breakpoints spatially

--- R/bpPheno.R
+++ R/bpPheno.R
@@ -5,6 +5,7 @@
 #' @param formula See \link{breakpoints}
 #' @param breaks See \link{breakpoints}
 #' @param h See \link{breakpoints}
+#' @param nbreaks Numeric, maximum theoretical number of breaks (for spatial implementation)
 #' 
 #' 
 #' @import strucchange
@@ -15,14 +16,39 @@
 #' 
 
 
-bpPheno <- function(x, order=1, formula = response ~ trend + harmon, breaks = NULL, h = 0.15) {
-    ts <- bfastts(data = x, dates = index(x), type = 'irregular')
-    pp <- bfastpp(ts, order = order)
-    breaks <- breakpoints(formula = formula, data = pp, breaks = breaks, h = h)
-    out <- list(df = pp,
-                breaks = breaks)
-    class(out) <- 'bpPheno'
-    return(out)
+bpPheno <- function(x, order=1, formula = response ~ trend + harmon, breaks = NULL, h = 0.15, nbreaks = NULL, mc.cores = 1) {
     
-    # TODO: Expand that function to export regression lines coefficients between segments
+    # def function bp2df
+    bp2df <- function(bpOut, pp, nbreaks = NULL) {
+        if (!is.null(nbreaks)) {
+            mat <- matrix(nrow = 1, ncol = nbreaks + 1)
+            df <- as.data.frame(mat)
+            colnames(df) <- c('nbreaks', sprintf('break%d', c(1:nbreaks)))
+            if(class(bpOut) == 'try-error') {
+                nBreaksOut <- -1 # So that -1 breaks means that an error occured
+            } else {
+                nBreaksOut <- length(bpOut$breakpoints)
+                df[2:nBreaksOut + 1] <- pp$time[breaks$breakpoints]
+            }
+            df[,'nbreaks'] <- nBreaksOut
+        } else {
+            stop('specify a maximum number of breaks ((Nb of observation * h) -1)')
+        }
+        
+    }
+    
+    # def function bp
+    bp <- function(ts, order, formula, breaks, h) {
+        pp <- bfastpp(ts, order = order)
+        bpOut <- try(breakpoints(formula = formula, data = pp, breaks = breaks, h = h))
+        bp2df(bpOut, pp, nbreaks = nbreaks) # Returned variable
+    }
+    
+    # def function wrapping the two above functions
+    
+    ts <- bfastts(data = x, dates = index(x), type = 'irregular')
+    out <- mclapply(X = ts, FUN = bp, mc.cores = mc.cores, order = order, formula = formula, breaks = breaks, h = h)
+    # Convert list of df to df
+    do.call(rbind, out)
 }
+
---
+++ R/bpPhenoSp.R
@@ -0,0 +1,69 @@
+#' Runs breakpoints for a spatial subset with aggregation
+#' 
+#' @description Runs \link{breakpoints} on a rasterBrick object for a set of locations, determined by an object of class \link{Spatial-class}.
+#' 
+#' @param x A rasterBrick or rasterStack, ideally with time written to the z dimension. In case time is not written to the z dimension, the \code{dates=} argument has to be supplied (see \link{zooExtract})
+#' @param y A SpatialPoints, SpatialPointsDataFrame, SpatialPolygons, SpatialPolygonsDataFrame, SpatialLines, SpatialLinesDataFrame, or extent. \link{breakpoints} will be ran at these locations. In case each feature of the object covers several pixels (typically SpatialPolygons(DataFrames), SpatialLines(DataFrames) and extent), an aggregation function (\code{fun=}) has to be supplied (see \link{extract}).
+#' @param formula See \code{\link{breakpoints}}
+#' @param order See \code{\link{bfastpp}}
+#' @param breaks See \code{\link{breakpoints}}
+#' @param h See \code{\link{breakpoints}}
+#' @param mc.cores Numeric NUmber of cores to use (for parallel processing)
+#' @param ... Arguments to be passed to \link{zooExtract}
+#' 
+#' @author Loic Dutrieux
+#' 
+#' @examples
+#' # Load data
+#' data(tura)
+#' 
+#' # 1- SpatialPoints case
+#' # Generate SpatialPoints
+#' sp <- sampleRegular(x = tura, size = 20, sp=TRUE)
+#' 
+#' # Run 
+#' 
+#' # 2 - SpatialPolygons case
+#' data(turaSp)
+#'
+#' 
+#' @import raster
+#' @import sp
+#' @import strucchange
+#' @import bfast
+#' @import zoo
+#' 
+#' @export
+
+bpPhenoSp <- function(x, y, formula = response ~ trend + harmon, order = 1, h = 0.1, breaks = NULL, mc.cores = 1, ...) {
+    
+    ts <- zooExtract(x, y, ...)
+    
+    # Get max segments
+    nl <- nlayers(x)
+    if (!is.null(breaks)) {
+        segMax <- breaks + 1
+    } else {
+        segMax <- ceiling(nl * h)
+    }
+    
+    
+    # Run function (must apply to a zoo object with multiple ts and return a dataframe)
+    bp <- bpPheno(x = ts, order = order, formula = formula, breaks = breaks, h = h, nbreaks = segMax, mc.cores = mc.cores)
+    
+    
+    
+    if(inherits(y, 'SpatialPoints')) {
+        y <- SpatialPoints(y) # Not sure if that is necessary
+        out <- SpatialPointsDataFrame(y, data = bp)
+    } else if(inherits(y, 'SpatialPolygons')) {
+        out <- SpatialPolygonsDataFrame(y, data = bp, match.ID = FALSE)
+    } else if(inherits(y, 'SpatialLines')) {
+        out <- SpatialLinesDataFrame(y, data = bp, match.ID = FALSE)
+    } else if(class(y) == 'extent') {
+        y <- as(y, 'SpatialPolygons')
+        out <- SpatialPolygonsDataFrame(y, data = bp, match.ID = FALSE)
+        proj4string(out) <- CRS(projection(x))
+    }
+    return(out)
+}
