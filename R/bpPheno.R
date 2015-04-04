1521e06fab43ceb465151e4cd66f29990c3a2654
Loïc <loic.dutrieux@gmail.com>
2015-04-04 17:35:27 +0200

    First attempt to return segments characteristics out of bpPheno (untested)

--- R/bpPheno.R
+++ R/bpPheno.R
@@ -21,15 +21,40 @@ bpPheno <- function(x, order=1, formula = response ~ trend + harmon, breaks = NU
     # def function bp2df
     bp2df <- function(bpOut, pp, nbreaks = NULL) {
         if (!is.null(nbreaks)) {
-            mat <- matrix(nrow = 1, ncol = nbreaks + 1)
+            mat <- matrix(nrow = 1, ncol = 1 + nbreaks + (nbreaks + 1) * 5)
             df <- as.data.frame(mat)
-            colnames(df) <- c('nbreaks', sprintf('break%d', c(1:nbreaks)))
+            colnames(df) <- c('nbreaks', sprintf('break%d', c(1:nbreaks)), unlist(lapply(c(0:nbreaks), function(x) sprintf(c('meanSeg%d', 'slopeSeg%d', 'yBeginSeg%d', 'yEndSeg%d', 'durationSeg%d'), x))))
             if(class(bpOut) == 'try-error') {
                 nBreaksOut <- -1 # So that -1 breaks means that an error occured
             } else {
                 if(!is.na(bpOut$breakpoints)) {
+                    # Input number of breaks to output df
                     nBreaksOut <- length(bpOut$breakpoints)
+                    # Input breaks timing into output df
                     df[1,2:(nBreaksOut + 1)] <- pp$time[bpOut$breakpoints]
+                    # Input Segments properties
+                    segments <- c(pp$time[c(1,bp$breakpoints, nrow(pp))])
+                    for (i in 1:(length(segments) - 1)) {
+                        # To deal with begining of time-series boundary
+                        if (i == 1) {
+                            subDf <- subset(pp, time <= segments[i + 1])
+                        } else {
+                            subDf <- subset(pp, time <= segments[i + 1] & time > segments[i]) 
+                        }
+                        m <- mean(subDf$response, na.rm = TRUE)
+                        model <- lm(response ~ trend, data = subDf)
+                        sl <- model$coefficients[2]
+                        n <- nrow(subDf)
+                        yBegin <- predict(model, newdata = subDf[1,]) # Beginning of segment (!= to intersection with break)
+                        yEnd <- predict(model, newdata = subDf[n,]) # End of segment (== intersection with breakLine)
+                        duration <- diff(range(subDf$time))
+                        segNum <- i - 1
+                        colNames0 <- c('meanSeg', 'slopeSeg', 'yBeginSeg', 'yEndSeg', 'durationSeg')
+                        colNames <- sprintf('%s%d', colNames0, i)
+                        # Fill dataframe 
+                        df[1, colNames] <- c(m, sl, yBegin, yEnd, duration)
+                    }
+                    
                 } else {
                     nBreaksOut <- 0
                 }
